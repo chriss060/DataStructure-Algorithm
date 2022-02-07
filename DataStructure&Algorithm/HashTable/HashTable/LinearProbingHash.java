a3d9357c85fc0bc761ba6a4e301a29f9b479c5aa
chriss060 <84179278+chriss060@users.noreply.github.com>
2022-02-07 17:53:27 +0900

    Create LinearProbingHash.java

---
+++ DataStructure&Algorithm/HashTable/HashTable/LinearProbingHash.java
@@ -0,0 +1,75 @@
+public class myHashLinear {
+
+    public Slot[] hashTable;
+    
+    public myHashLinear(Integer size) {
+        this.hashTable = new Slot[size];
+    }
+    
+    public class Slot {
+        String key;
+        String value;
+        Slot(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
+    public int hashFunc(String key) {
+        return (int)(key.charAt(0)) % this.hashTable.length;
+    }
+    
+    public boolean saveData(String key, String value) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            if (this.hashTable[address].key == key) {
+                this.hashTable[address].value = value;
+                return true;
+            } else {
+                //Linear Probing 기법으로 다음 빈 슬롯에 data 추가
+                Integer currAddress = address + 1;
+                while (this.hashTable[currAddress] != null) {
+                    if (this.hashTable[currAddress].key == key) {
+                        this.hashTable[currAddress].value = value;
+                        return true;
+                    } else {
+                        currAddress++;
+                        if (currAddress >= this.hashTable.length) {
+                            return false;
+                        }                        
+                    }
+                }
+                this.hashTable[currAddress] = new Slot(key, value);
+                return true;
+            }
+        } else {
+            this.hashTable[address] = new Slot(key, value);
+        }
+        return true;
+    }
+    
+    public String getData(String key) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            if (this.hashTable[address].key == key) {
+                return this.hashTable[address].value;
+            } else {
+
+                Integer currAddress = address; 
+                while (this.hashTable[currAddress] != null) {
+                    if (this.hashTable[currAddress].key == key) {
+                        return this.hashTable[currAddress].value;
+                    } else {
+                        currAddress++;
+                        if (currAddress >= this.hashTable.length) {
+                            return null;
+                        }
+                    }
+                }
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+}
