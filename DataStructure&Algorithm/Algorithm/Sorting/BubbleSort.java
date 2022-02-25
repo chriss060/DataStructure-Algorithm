d1ead1ac8730ad58342db57d7babf4f1cd6cac32
chriss060 <84179278+chriss060@users.noreply.github.com>
2022-02-25 18:58:35 +0900

    Create BubbleSort.java

---
+++ DataStructure&Algorithm/Algorithm/Sorting/BubbleSort.java
@@ -0,0 +1,40 @@
+package BubbleSort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BubbleSort {
+
+	public ArrayList<Integer> sort(ArrayList<Integer> dataList){
+		
+		for(int index=0; index<dataList.size()-1; index++) {
+			
+			boolean swap = false;
+			
+			for(int index2=0; index2<dataList.size()-1-index; index2++) {
+				if(dataList.get(index2)> dataList.get(index2+1)) {
+					Collections.swap(dataList, index2, index2+1);
+					swap = true;
+				}
+			}
+			
+			if(swap == false) { //이미 로직이 경우에 따라 정렬된 경우 break;
+				break;
+			}
+		}
+		
+		return dataList;
+	}
+
+
+public static void main(String[] args) {
+	
+	ArrayList<Integer> testData = new ArrayList<Integer>();
+	for (int i = 0; i < 100; i++) {
+	    testData.add((int)(Math.random() * 100));
+	}
+	BubbleSort bSort = new BubbleSort();
+	bSort.sort(testData);
+}
+
+}
