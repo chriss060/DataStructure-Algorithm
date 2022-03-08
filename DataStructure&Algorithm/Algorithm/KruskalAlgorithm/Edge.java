2e0ae274f5ce936fbc7ff966fec8933457719f48
이해현 <84179278+chriss060@users.noreply.github.com>
2022-03-09 00:19:57 +0900

    Create Edge.java

---
+++ DataStructure&Algorithm/Algorithm/KruskalAlgorithm/Edge.java
@@ -0,0 +1,25 @@
+package KruscalAlgorithm;
+
+
+public class Edge implements Comparable<Edge>{
+	
+	public int weight;
+	public String nodeV;
+	public String nodeU;
+	
+	public Edge(int weight, String nodeV, String nodeU) {
+		this.weight = weight;
+		this.nodeV = nodeV;
+		this.nodeU = nodeU;
+	}
+	
+	public String toString() {
+		return weight+" , "+nodeV+" , "+ nodeU;
+	}
+	
+	@Override
+	public int compareTo(Edge edge) {
+		return this.weight - edge.weight;
+	}
+
+}
